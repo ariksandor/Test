4eb6e0bbe34a2fd07885eb9d87d3b6046b3b85da
ariks <ariksandor@gmail.com>
2025-03-22 15:13:22 +0300

    Крайняя правка 4 задач

--- task_4.py
+++ task_4.py
@@ -2,13 +2,13 @@
 
 def calculate_square_triangle(cat_1, cat_2):
     square = 0.5 * cat_1 * cat_2
-    return f'Площадь прямоугольного треугольника: {square}'
-input_cat_1, input_cat_2  = input('\nВведите длину одного катета:\t') , input('\nВведите длину другого катета:\t')
+    return f'\nПлощадь прямоугольного треугольника: {square}'
+input_cat_1, input_cat_2  = input('Введите длину одного катета:\t') , input('Введите длину другого катета:\t')
 
 while type(input_cat_1) != float:
      try:
          input_cat_1 = float(input_cat_1)
          input_cat_2 = float(input_cat_2)
      except ValueError:
-         input_cat_1, input_cat_2  = input('\nВведите корректную длину одного катета:\t') , input('\nВведите корректную длину другого катета:\t')
+         input_cat_1, input_cat_2  = input('Введите корректную длину одного катета:\t') , input('Введите корректную длину другого катета:\t')
 print(calculate_square_triangle(input_cat_1, input_cat_2))
