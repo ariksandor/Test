68efb0d1f46bfccb81416c694e773010dc99a0b0
ariks <ariksandor@gmail.com>
2025-03-24 15:17:55 +0300

    Решаю 8 задачу

---
+++ task_8.py
@@ -0,0 +1,28 @@
+goods = {
+'Лампа': '12345',
+'Стол': '23456',
+'Диван': '34567',
+'Стул': '45678',
+} #dict_1
+store = {
+'12345': [
+{'quantity': 27, 'price': 42},
+],
+'23456': [
+{'quantity': 22, 'price': 510},
+{'quantity': 32, 'price': 520},
+],
+'34567': [
+{'quantity': 2, 'price': 1200},
+{'quantity': 1, 'price': 1150},
+],
+'45678': [
+{'quantity': 50, 'price': 100},
+{'quantity': 12, 'price': 95},
+{'quantity': 43, 'price': 97},
+],
+} #list_1 dict_2
+def print_cost_dicts(dict_1, dict_2):
+    for key, value in dict_1.items():
+        print(f'{key} - {dict_2[value][0]}')
+print_cost_dicts(goods, store)
